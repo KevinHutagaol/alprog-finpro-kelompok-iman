9ae7561e9da4f0e4094d63021708483e6d9d77fe
Kevin Imanuel <kevimanuel12345@gmail.com>
2025-05-22 07:29:36 +0700

    working example get all process from pc

--- client/src/performance_counter.cpp
+++ client/src/performance_counter.cpp
@@ -1,85 +1,90 @@
-// This program needs only the essential Windows header files.
-#define WIN32_LEAN_AND_MEAN 1
-
 #include <windows.h>
 #include <cstdio>
 #include <pdh.h>
 #include <pdhmsg.h>
-#include <io.h>
-#include <fcntl.h>
+#include <iostream>
+#include <memory>
+#include <ostream>
+#include <winstring.h>
+#include <string>
+#include <vector>
 
 
-#pragma comment(lib, "pdh.lib")
+CONST LPCWSTR COUNTER_OBJECT = L"Process";
 
-CONST LPCWSTR  COUNTER_OBJECT = L"Process";
+std::string ConvertLPCWSTRToString(LPCWSTR lpcwszStr) {
+    if (lpcwszStr == nullptr || *lpcwszStr == L'\0') return "";
+    int strLength
+            = WideCharToMultiByte(CP_UTF8, 0, lpcwszStr, -1,
+                                  nullptr, 0, nullptr, nullptr);
+    std::string str(strLength, 0);
+    WideCharToMultiByte(CP_UTF8, 0, lpcwszStr, -1, &str[0],
+                        strLength, nullptr, nullptr);
+    return str;
+}
 
 int main() {
-    int prev_mode = _setmode(_fileno(stdout), _O_U16TEXT);
-
-    wprintf(L"prev mode: %d\n", prev_mode);
-
     PDH_STATUS status = ERROR_SUCCESS;
-    LPWSTR pwsCounterListBuffer = nullptr;
+    std::vector<wchar_t> pwsCounterListBuffer;
     DWORD dwCounterListSize = 0;
-    LPWSTR pwsInstanceListBuffer = nullptr;
+    std::vector<wchar_t> pwsInstanceListBuffer;
     DWORD dwInstanceListSize = 0;
     LPWSTR pTemp = nullptr;
 
     // Determine the required buffer size for the data.
     status = PdhEnumObjectItemsW(
-        nullptr,                    // real-time source
-        nullptr,                    // local machine
-        COUNTER_OBJECT,             // object to enumerate
-        pwsCounterListBuffer,       // pass NULL and 0
-        &dwCounterListSize,         // to get required buffer size
-        pwsInstanceListBuffer,
+        nullptr, // real-time source
+        nullptr, // local machine
+        COUNTER_OBJECT, // object to enumerate
+        nullptr, // pass NULL and 0
+        &dwCounterListSize, // to get required buffer size
+        nullptr,
         &dwInstanceListSize,
-        PERF_DETAIL_WIZARD,         // counter detail level
+        PERF_DETAIL_WIZARD, // counter detail level
         0);
 
-    if (status == PDH_MORE_DATA) {
-        // Allocate the buffers and try the call again.
-        pwsCounterListBuffer = static_cast<LPWSTR>(malloc(dwCounterListSize * sizeof(WCHAR)));
-        pwsInstanceListBuffer = static_cast<LPWSTR>(malloc(dwInstanceListSize * sizeof(WCHAR)));
-
-        if (nullptr != pwsCounterListBuffer && nullptr != pwsInstanceListBuffer) {
-            status = PdhEnumObjectItemsW(
-                nullptr, // real-time source
-                nullptr, // local machine
-                COUNTER_OBJECT, // object to enumerate
-                pwsCounterListBuffer,
-                &dwCounterListSize,
-                pwsInstanceListBuffer,
-                &dwInstanceListSize,
-                PERF_DETAIL_WIZARD, // counter detail level
-                0);
-
-            if (status == ERROR_SUCCESS) {
-                wprintf(L"Counters that the Process objects defines:\n\n");
-
-                for (pTemp = pwsCounterListBuffer; *pTemp != L'\0'; pTemp += wcslen(pTemp) + 1) {
-                    wprintf(L"%s\n", pTemp);
-                }
-
-                wprintf(L"\nInstances of the Process object:\n\n");
-
-                for (pTemp = pwsInstanceListBuffer; *pTemp != L'\0'; pTemp += wcslen(pTemp) + 1) {
-                    wprintf(L"%s\n", pTemp);
-                }
-            } else {
-                wprintf(L"Second PdhEnumObjectItems failed with 0x%x.\n", status);
-            }
-        } else {
-            wprintf(L"Unable to allocate buffers.\n");
-            status = ERROR_OUTOFMEMORY;
-        }
-    } else {
-        wprintf(L"\nPdhEnumObjectItems failed with 0x%x.\n", status);
+    if (status != PDH_MORE_DATA) {
+        std::cout << "\nPdhEnumObjectItems failed with 0x" << std::hex << status << std::endl;
+        return 1;
     }
 
-    if (pwsCounterListBuffer != nullptr)
-        free(pwsCounterListBuffer);
+    pwsCounterListBuffer.resize(dwCounterListSize);
+    pwsInstanceListBuffer.resize(dwInstanceListSize);
 
-    if (pwsInstanceListBuffer != nullptr)
-        free(pwsInstanceListBuffer);
+    if (pwsCounterListBuffer.empty() || pwsInstanceListBuffer.empty()) {
+        std::cout << "Unable to allocate buffers." << std::endl;
+        status = ERROR_OUTOFMEMORY;
+        return 1;
+    }
+
+    status = PdhEnumObjectItemsW(
+        nullptr, // real-time source
+        nullptr, // local machine
+        COUNTER_OBJECT, // object to enumerate
+        pwsCounterListBuffer.data(),
+        &dwCounterListSize,
+        pwsInstanceListBuffer.data(),
+        &dwInstanceListSize,
+        PERF_DETAIL_WIZARD, // counter detail level
+        0);
+
+    if (status != ERROR_SUCCESS) {
+        std::cout << "Second PdhEnumObjectItems failed with 0x" << std::hex << status << std::endl;
+    }
+
+    std::cout << "Counters that the Process objects defines:\n\n";
+
+    for (pTemp = pwsCounterListBuffer.data(); *pTemp != L'\0'; pTemp += wcslen(pTemp) + 1) {
+        std::string tempStr = ConvertLPCWSTRToString(pTemp);
+        std::cout << tempStr << std::endl;
+    }
+
+    std::cout << "\nInstances of the Process object:\n\n";
+
+    for (pTemp = pwsInstanceListBuffer.data(); *pTemp != L'\0'; pTemp += wcslen(pTemp) + 1) {
+        std::string tempStr = ConvertLPCWSTRToString(pTemp);
+        std::cout << tempStr << std::endl;
+    }
+
+    return 0;
 }
