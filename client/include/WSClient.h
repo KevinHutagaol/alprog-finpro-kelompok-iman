b1370c8fd64193870ec1592d059ee8802a2dd309
Kevin Imanuel <kevimanuel12345@gmail.com>
2025-06-11 02:09:20 +0700

    removed unnecessary comments, refactored to use boost::beast websocekts coroutines, finished implementation (not yet tested)

--- client/include/WSClient.h
+++ client/include/WSClient.h
@@ -1,83 +1,49 @@
 #ifndef WSCLIENT_H
 #define WSCLIENT_H
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/spawn.hpp>
 #include <string>
-#include <functional>
-#include <memory>
-#include <condition_variable>
 
-/**
- * @class WSClient
- * @brief A WebSocket client class that can be accessed by any other classes
- *
- * This class provides a generic WebSocket client implementation that can be used
- * to establish WebSocket connections, send messages, and receive messages from a WebSocket server.
- */
 class WSClient {
 public:
-    /**
-     * @brief Constructor for WSClient
-     */
-    WSClient();
+    WSClient(boost::asio::io_context &ioc, const std::string &host_, const std::string &port_);
 
-    /**
-     * @brief Destructor for WSClient
-     */
     ~WSClient();
 
-    /**
-     * @brief Connect to a WebSocket server
-     * @param uri The URI of the WebSocket server
-     * @return true if connection is successful, false otherwise
-     */
-    bool connect(const std::string& uri);
+    void connect();
 
-    /**
-     * @brief Disconnect from the WebSocket server
-     */
     void disconnect();
 
-    /**
-     * @brief Send a message to the WebSocket server
-     * @param message The message to send
-     * @return true if message is sent successfully, false otherwise
-     */
-    bool send(const std::string& message);
+    void send(const std::string &message);
 
-    /**
-     * @brief Set callback function for when a message is received
-     * @param callback The function to call when a message is received
-     */
-    void setOnMessageCallback(std::function<void(const std::string&)> callback);
+    [[nodiscard]] bool isConnected() const;
 
-    /**
-     * @brief Set callback function for when connection is established
-     * @param callback The function to call when connection is established
-     */
-    void setOnConnectCallback(std::function<void()> callback);
+    void setOnMessageCallback(std::function<void(const std::string &)> on_message_handler);
 
-    /**
-     * @brief Set callback function for when connection is closed
-     * @param callback The function to call when connection is closed
-     */
-    void setOnCloseCallback(std::function<void(const std::string&)> callback);
+    void setOnConnectCallback(std::function<void(const boost::beast::error_code &)> on_connect_handler);
 
-    /**
-     * @brief Set callback function for when an error occurs
-     * @param callback The function to call when an error occurs
-     */
-    void setOnErrorCallback(std::function<void(const std::string&)> callback);
-
-    /**
-     * @brief Check if the client is connected to the server
-     * @return true if connected, false otherwise
-     */
-    bool isConnected() const;
+    void setOnSendCallback(std::function<void(const boost::beast::error_code &)> on_send_handler);
 
 private:
-    // WebSocket internal implementation details
-    class WSClientImpl;
-    std::unique_ptr<WSClientImpl> pImpl;
+    std::function<void(const std::string &)> on_message_callback_;
+    std::function<void(const boost::beast::error_code &)> on_connect_callback_;
+    std::function<void(const boost::beast::error_code &)> on_send_callback_;
+
+    std::string host_;
+    std::string port_;
+
+    boost::asio::ip::tcp::resolver resolver_;
+    boost::asio::io_context &ioc_;
+    boost::beast::websocket::stream<boost::beast::tcp_stream> ws_;
+    boost::beast::flat_buffer buffer_;
+
+    std::atomic<bool> is_connected_{false};
+
+    void fail(const boost::beast::error_code &ec, char const *what);
+
+    void read_loop();
 };
 
 #endif // WSCLIENT_H
--- client/src/WSClient.cpp
+++ client/src/WSClient.cpp
@@ -1,101 +1,132 @@
 #include "WSClient.h"
+
 #include <iostream>
+#include <utility>
 
 
-class WSClient::WSClientImpl {
-public:
-    WSClientImpl() : connected(false) {}
+WSClient::WSClient(boost::asio::io_context &ioc_, const std::string &host_, const std::string &port_): host_(host_),
+    port_(port_), resolver_(ioc_), ioc_(ioc_), ws_(ioc_) {
+}
 
-    bool connect(const std::string& uri) {
-        std::cout << "Connecting to: " << uri << std::endl;
-        connected = true;
-        if (onConnectCallback) {
-            onConnectCallback();
+WSClient::~WSClient() {
+}
+
+void WSClient::connect() {
+    boost::asio::spawn(this->ioc_, [this](boost::asio::yield_context yield) {
+        boost::beast::error_code ec;
+
+        const auto results = this->resolver_.async_resolve(this->host_, this->port_, yield[ec]);
+        if (ec) {
+            return fail(ec, "resolve");
         }
-        return true;
-    }
+        boost::beast::get_lowest_layer(this->ws_).expires_after(std::chrono::seconds(30));
 
-    void disconnect() {
-        if (connected) {
-            connected = false;
-            if (onCloseCallback) {
-                onCloseCallback("Connection closed normally");
-            }
-        }
-    }
-
-    bool send(const std::string& message) {
-        if (!connected) {
-            if (onErrorCallback) {
-                onErrorCallback("Cannot send message: not connected");
-            }
-            return false;
+        const auto ep = boost::beast::get_lowest_layer(this->ws_).async_connect(results, yield[ec]);
+        if (ec) {
+            return fail(ec, "connect");
         }
 
-        std::cout << "Sending message: " << message << std::endl;
-        return true;
-    }
+        std::string host_with_port = this->host_ + ':' + std::to_string(ep.port());
 
-    bool isConnected() const {
-        return connected;
-    }
+        boost::beast::get_lowest_layer(this->ws_).expires_never();
 
-    void setOnMessageCallback(std::function<void(const std::string&)> callback) {
-        onMessageCallback = callback;
-    }
+        this->ws_.set_option(
+            boost::beast::websocket::stream_base::timeout::suggested(
+                boost::beast::role_type::client));
 
-    void setOnConnectCallback(std::function<void()> callback) {
-        onConnectCallback = callback;
-    }
+        this->ws_.set_option(boost::beast::websocket::stream_base::decorator(
+            [](boost::beast::websocket::request_type &req) {
+                req.set(boost::beast::http::field::user_agent,
+                        std::string(BOOST_BEAST_VERSION_STRING) +
+                        " websocket-client-coro");
+            }));
 
-    void setOnCloseCallback(std::function<void(const std::string&)> callback) {
-        onCloseCallback = callback;
-    }
+        this->ws_.async_handshake(host_with_port, "/", yield[ec]);
+        if (ec) {
+            return fail(ec, "handshake");
+        }
 
-    void setOnErrorCallback(std::function<void(const std::string&)> callback) {
-        onErrorCallback = callback;
-    }
+        this->is_connected_.store(true);
+        std::cout << "Connected to: " << host_with_port << std::endl;
+        read_loop();
 
-private:
-    std::atomic<bool> connected;
-    std::function<void(const std::string&)> onMessageCallback;
-    std::function<void()> onConnectCallback;
-    std::function<void(const std::string&)> onCloseCallback;
-    std::function<void(const std::string&)> onErrorCallback;
-};
-
-WSClient::WSClient() : pImpl(std::make_unique<WSClientImpl>()) {}
-
-WSClient::~WSClient() = default;
-
-bool WSClient::connect(const std::string& uri) {
-    return pImpl->connect(uri);
+        if (this->on_connect_callback_) {
+            this->on_connect_callback_({});
+        }
+    });
 }
 
 void WSClient::disconnect() {
-    pImpl->disconnect();
+    if (!this->is_connected_.load()) {
+        return;
+    }
+
+    // change to ioc_ thread (which is created from connect fn)
+    boost::asio::post(this->ioc_, [this]() {
+        this->ws_.async_close(boost::beast::websocket::close_code::normal, [this](auto ec) {
+            this->is_connected_.store(false);
+            if (ec) {
+                return fail(ec, "close");
+            }
+        });
+    });
 }
 
-bool WSClient::send(const std::string& message) {
-    return pImpl->send(message);
+void WSClient::send(const std::string &message) {
+    const auto shared_message = std::make_shared<std::string>(message);
+
+    boost::asio::post(this->ioc_, [this, shared_message]() {
+        this->ws_.async_write(*shared_message, [this](auto ec, auto) {
+            if (this->on_send_callback_) {
+                on_send_handler_(ec);
+            }
+
+            if (ec) {
+                return fail(ec, "write");
+            }
+        });
+    });
 }
 
-void WSClient::setOnMessageCallback(std::function<void(const std::string&)> callback) {
-    pImpl->setOnMessageCallback(callback);
+void WSClient::read_loop() {
+    // does not need boost::asio::post because it is only called from connect
+    // which is already in the thread
+    this->ws_.async_read(this->buffer_, [this](auto ec, auto) {
+        if (ec == boost::beast::websocket::error::closed) {
+            is_connected_ = false;
+            std::cout << "Connection closed by peer." << std::endl;
+            return;
+        }
+        if (ec) {
+            return fail(ec, "read");
+        }
+        if (this->on_message_callback_) {
+            this->on_message_callback_(boost::beast::buffers_to_string(this->buffer_.data()));
+        }
+
+        this->buffer_.consume(this->buffer_.size());
+
+        read_loop();
+    });
 }
 
-void WSClient::setOnConnectCallback(std::function<void()> callback) {
-    pImpl->setOnConnectCallback(callback);
-}
-
-void WSClient::setOnCloseCallback(std::function<void(const std::string&)> callback) {
-    pImpl->setOnCloseCallback(callback);
-}
-
-void WSClient::setOnErrorCallback(std::function<void(const std::string&)> callback) {
-    pImpl->setOnErrorCallback(callback);
-}
 
 bool WSClient::isConnected() const {
-    return pImpl->isConnected();
+    return this->is_connected_.load();
+}
+
+void WSClient::setOnMessageCallback(std::function<void(const std::string &)> on_message_handler) {
+    this->on_message_callback_ = std::move(on_message_handler);
+}
+
+void WSClient::setOnConnectCallback(std::function<void(const boost::beast::error_code &)> on_connect_handler) {
+    this->on_connect_callback_ = std::move(on_connect_handler);
+}
+
+void WSClient::setOnSendCallback(std::function<void(const boost::beast::error_code &)> on_send_handler) {
+    this->on_send_callback_ = std::move(on_send_handler);
+}
+
+void WSClient::fail(const boost::beast::error_code &ec, char const *what) {
+    std::cerr << what << ": " << ec.message() << "\n";
 }
